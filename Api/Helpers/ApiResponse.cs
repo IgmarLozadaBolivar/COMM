a928f3efe9c9d9aaa285d3b7eebad28a0ed889ae
Alejomdi193 <mdiegoalejandro17@gmail.com>
2024-04-01 12:38:10 -0500

    error 403

--- Api/Helpers/ApiResponse.cs
+++ Api/Helpers/ApiResponse.cs
@@ -21,6 +21,7 @@ public class ApiResponse
         {
             400 => "Has realizado una petición incorrecta.",
             401 => "Usuario no autorizado.",
+            403 => "Usuario no autorizado.",
             404 => "El recurso que has intentado solicitar no existe.",
             405 => "Este método HTTP no está permitido en el servidor.",
             500 => "Error en el servidor. No eres tú, soy yo. Comunícate con el administrador XD.",
